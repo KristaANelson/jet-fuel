0a094836c24d0359f24fdca33ec0d52233e1b116
KristaANelson <krista.a.nelson@gmail.com>
2015-03-27 10:57:25 -0500

    url shortens

---
+++ jet-fuel/app/controllers/urls_controller.rb
@@ -0,0 +1,21 @@
+class UrlsController < ApplicationController
+  def create
+    url = Url.new(url_params)
+    if url.save
+      redirect_to url_path(url)
+    else
+      redirect_to root_path
+    end
+  end
+
+  def show
+    require 'pry'; binding.pry
+    @url = Url.find(params[:id])
+  end
+
+private
+
+  def url_params
+    params.require(:url).permit(:full_url)
+  end
+end
--- jet-fuel/app/models/url.rb
+++ jet-fuel/app/models/url.rb
@@ -1,3 +1,9 @@
 class Url < ActiveRecord::Base
-  validates :full_url, presence: true
+  before_validation :shorten_url
+  validates :full_url, presence: true, uniqueness: true
+  validates :short_url, presence: true, uniqueness: true
+
+  def shorten_url
+    self.short_url = Time.new.strftime("%H%M%S")
+  end
 end
