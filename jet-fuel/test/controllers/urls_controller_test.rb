c5b116385edea9792aa731109f595dcb5c5c3a67
KristaANelson <krista.a.nelson@gmail.com>
2015-03-29 03:53:57 -0500

    hound fixes

--- jet-fuel/test/controllers/urls_controller_test.rb
+++ jet-fuel/test/controllers/urls_controller_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class UrlsControllerTest < ActionController::TestCase
   test "should create url" do
     assert_difference("Url.count") do
-      post :create, url: {full_url: "https://mail.google.com"}
+      post :create, url: { full_url: "https://mail.google.com" }
     end
     assert_redirected_to url_path(Url.last)
   end
